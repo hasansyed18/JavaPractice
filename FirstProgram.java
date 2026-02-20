8150fba324ef653ca2fb2445d83b8b62f85ecdc2
Hasan <hasan.syed.hs18@gmail.com>
2026-02-20 05:51:48 +0530

    Updated FirstProgram with additional print statement

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,5 +1,7 @@
 public class FirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub! This is Hasan.");
+        System.out.println("Hello GitHub!");
+        System.out.println("Learning Git step by step.");
+        System.out.println("This is my first Java program.");
     }
 }
