56fbdb3666356ff0fb3c9761608dd8467eb49676
Hasan <hasan.syed.hs18@gmail.com>
2026-06-06 03:27:36 +0530

    Added Taking Input from user Program

---
+++ TakeInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class TakeInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER YOUR NAME");
+        String name = sc.nextLine();
+        System.out.println("HELLO " +name+ "!");
+    }
+}
