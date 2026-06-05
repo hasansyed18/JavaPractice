8c2465b3d4fda929da490e4545f96ee050eb27ef
Hasan <hasan.syed.hs18@gmail.com>
2026-06-06 02:05:35 +0530

    Added basic Java programs

--- FirstProgram.java
+++ FirstProgram.java
@@ -2,6 +2,6 @@ public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
         System.out.println("Learning Git step by step.");
-        System.out.println("This is my first Java program.");
+        System.out.println("My first Java program.");
     }
 }
