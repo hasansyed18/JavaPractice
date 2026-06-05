29237f94220b78136766544000fa68b5fd39bf95
Hasan <hasan.syed.hs18@gmail.com>
2026-06-06 03:39:20 +0530

    Added SUM of two numbers program

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num1, num2;
+        System.out.println("ENTER THE FIRST NUMBER");
+        num1 = sc.nextDouble();
+        System.out.println("ENTER THE SECOND NUMBER");
+        num2 = sc.nextDouble();
+        System.out.println("Sum of the given two number are :"+(num1 + num2));
+    }
+}
