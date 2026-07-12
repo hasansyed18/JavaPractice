f4689c66f5fc5e2afda17d27e012c866c18180ed
Hasan <hasan.syed.hs18@gmail.com>
2026-07-13 00:39:28 +0530

    Modified Variables.java

--- Variables.java
+++ Variables.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Main {
+class Variables {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("ENTER YOUR NAME :");
